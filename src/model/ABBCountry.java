05b9569c735c4f64c4fe10ded0804f88623bc3fa
JLRO1812 <restrepitoj.l@gmail.com>
2020-05-14 00:11:40 -0500

    ABBContry class arranged and complete. The ABBCountryTest class was successfully completed and ended. Latest aesthetic fixes in fxml layout. algorithmic-olympics is fully functional. Completed

--- src/model/ABBCountry.java
+++ src/model/ABBCountry.java
@@ -10,30 +10,65 @@ public class ABBCountry {
 	
 	//Add ITERATIVE	
 	public void addIterative(long n) {
+		
 		NodoABB nodo = new NodoABB(n);
-		NodoABB current = top;
+		NodoABB father;
 		
-		boolean available = false;
+		boolean available;
 		
-		while(current!=null&&!available) {
-			if(current.getAmount()>n) {
-				current = current.getLeft();
-			}else if(current.getAmount()<n) {
-				current = current.getRigth();
-			}else {
-				available = true;
-			}	
-		}if(available)
-			current = nodo;
+		if(top == null) {
+			top = nodo;
+		}else {
+			NodoABB current = top;
+			available = true;
+			while(available) {
+				father = current;
+				if(nodo.getAmount()<current.getAmount()) {
+					current = current.getLeft();
+					if(current == null) {
+						father.setLeft(nodo);
+						available = false;
+					}
+				}else if(nodo.getAmount()>current.getAmount()) {
+					current = current.getRigth();
+					if(current == null) {
+						father.setRigth(nodo);
+						available = false;
+					}
+				}
+			}
+		}
 	}
 	
 	//Add RECURSIVE
 	public void addRecursive(long n) {
+		NodoABB nodo = new NodoABB(n);
 		
+		if(top == null) {
+			top = nodo;
+		}else {
+			NodoABB current = top;
+			addRecursive(nodo,current);
+		}
 	}
 	
-	public void addRecursive() {
-		
+	public void addRecursive(NodoABB nodo, NodoABB current) {
+		NodoABB father = current;
+		if(nodo.getAmount()<current.getAmount()) {
+			current = current.getLeft();
+			if(current == null) {
+				father.setLeft(nodo);
+			}else {
+				addRecursive(nodo, current);
+			}
+		}else if(nodo.getAmount()>current.getAmount()) {
+			current = current.getRigth();
+			if(current == null) {
+				father.setRigth(nodo);
+			}else {
+				addRecursive(nodo, current);
+			}
+		}
 	}
 	
 	//Search ITERATIVE
@@ -54,29 +89,234 @@ public class ABBCountry {
 	
 	//Search RECURSIVE
 	public boolean searchRecursive(long n) {
-		return false;
+		NodoABB current = top;
+		if(current.getAmount() != n) {
+			return searchRecursive(current, n);
+		}else {
+			return true;
+		}
 		
 	}
 	
-	public boolean searchRecursive(NodoABB aux, long n) {
-		return false;
-		
+	public boolean searchRecursive(NodoABB current, long n) {
+		if(current.getAmount()>n) {
+			current = current.getLeft();
+		}else if(current.getAmount()<n) {
+			current = current.getRigth();
+		}if(current != null) {
+			if(current.getAmount() == n) {
+				return true;
+			}else {
+				return searchRecursive(current, n);
+			}
+		}else 
+			return false;			
 	}
 	
 	//Delete ITERATIVE
 	public boolean deleteIterative(long n) {
-		return false;
+		NodoABB current = top;
+		NodoABB father = top;
 		
+		boolean turnLeft = true;
+		
+		while(current.getAmount()!=n) {
+			 father = current;
+			 
+			 if(current.getAmount()>n) {
+				 turnLeft = true;
+				 current =current.getLeft();
+			 }else if(current.getAmount()<n) {
+				 turnLeft = false;
+				 current =current.getRigth();
+			 }if(current == null) {
+				 return false;
+			 }
+		}
+		
+		if(current.getLeft() == null && current.getRigth() == null) {
+			
+			if(current == top) {
+				top = null;
+			}else {
+				if(turnLeft) {
+					father.setLeft(null);
+				}else {
+					father.setRigth(null);
+				}
+			}
+		}else {
+			
+			if(current.getRigth() == null) {
+				
+				if(current == top) {
+					top = current.getLeft();
+				}else {
+					if(turnLeft) {
+						father.setLeft(current.getLeft());
+					}else {
+						father.setRigth(current.getLeft());
+					}
+				}
+			}else {
+				if(current.getLeft()==null) {
+					if(current == top) {
+						top = current.getRigth();
+					}else {
+						if(turnLeft) {
+							father.setLeft(current.getRigth());
+						}else {
+							father.setRigth(current.getRigth());
+						}
+					}
+				}else {
+					NodoABB change = getChangeIterative(current);
+					if(current == top) {
+						top = change;
+					}else {
+						if(turnLeft) {
+							father.setLeft(change);
+						}else {
+							father.setRigth(change);
+						}
+					}change.setLeft(current.getLeft());
+				}
+			}
+		}return true;
+	}
+	
+	private NodoABB getChangeIterative(NodoABB obtain) {
+		NodoABB changeFather = obtain;
+		NodoABB change = obtain;
+		NodoABB current = obtain.getRigth();
+		
+		while(current != null) {
+			changeFather = change;
+			change = current;
+			current = current.getLeft();
+		}
+		
+		if(change != obtain.getRigth()) {
+			changeFather.setLeft(change.getRigth());
+			change.setRigth(obtain.getRigth());
+		}return change;
 	}
 	
 	//Delete RECURSIVE
 	public boolean deleteRecursive(long n) {
-		return false;
+		NodoABB[] nodos = {top, top};
+		if(top.getAmount() != n) {
+			nodos = deleteRecursive(nodos, n); 
+		}
 		
+		if(nodos == null) {
+			return false;
+		}
+		
+		NodoABB father = nodos[0];
+		NodoABB current = nodos[1];
+		
+		boolean turnLeft;
+		
+		if(father.getLeft() != null) {
+			if(father.getLeft().getAmount() == current.getAmount()) 
+				turnLeft = true;
+			else 
+				turnLeft = false;
+		}else
+			turnLeft = false;
+		
+		if(current.getLeft() == null && current.getRigth() == null) {
+			
+			if(current == top) {
+				top = null;
+			}else {
+				if(turnLeft) {
+					father.setLeft(null);
+				}else {
+					father.setRigth(null);
+				}
+			}
+		}else {
+			
+			if(current.getRigth() == null) {
+				
+				if(current == top) {
+					top = current.getLeft();
+				}else {
+					if(turnLeft) {
+						father.setLeft(current.getLeft());
+					}else {
+						father.setRigth(current.getLeft());
+					}
+				}
+			}else {
+				if(current.getLeft()==null) {
+					if(current == top) {
+						top = current.getRigth();
+					}else {
+						if(turnLeft) {
+							father.setLeft(current.getRigth());
+						}else {
+							father.setRigth(current.getRigth());
+						}
+					}
+				}else {
+					NodoABB change = getChangeRecursive(current, current, current.getRigth(), current);
+					if(current == top) {
+						top = change;
+					}else {
+						if(turnLeft) {
+							father.setLeft(change);
+						}else {
+							father.setRigth(change);
+						}
+					}change.setLeft(current.getLeft());
+				}
+			}
+		}return true;
 	}
 	
-	public boolean deleteRecursive() {
-		return false;
+	private NodoABB getChangeRecursive(NodoABB changeFather, NodoABB change, NodoABB current, NodoABB replace) {
+		changeFather = change;
+		change = current;
+		current = current.getLeft();
 		
+		if(current == null) {
+			if(change!=replace.getRigth()) {
+				changeFather.setLeft(change.getRigth());
+				change.setRigth(replace.getRigth());
+			}return replace;	
+		}else {
+			return getChangeRecursive(changeFather, change, current, replace);
+		}
+	}
+	
+	public NodoABB[] deleteRecursive(NodoABB[] nodos, long n) {
+		NodoABB father = nodos[0];
+		NodoABB current = nodos[1];
+		
+		father = current;
+		if(n<current.getAmount()) {
+			current = current.getLeft();
+		}else {
+			current = current.getRigth();
+		}
+		
+		NodoABB [] rNodos = {father, current};
+		
+		if(current == null) {
+			return null;
+		}else {
+			
+			if(current.getAmount() == n) 
+				return rNodos;
+			else 
+				return deleteRecursive(rNodos, n);
+		}
+	}
+	
+	public NodoABB getTop() {
+		return top;
 	}
 }
---
+++ test/model/ABBTest.java
@@ -0,0 +1,173 @@
+package model;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+public class ABBTest {
+	
+	private ABBCountry abbc;
+	
+	void setup1() {
+		abbc = new ABBCountry();
+	}
+	
+	void setup2() {
+		abbc = new ABBCountry();
+		
+		abbc.addIterative(20);
+		abbc.addIterative(40);
+		abbc.addIterative(60);
+		abbc.addIterative(80);
+		abbc.addIterative(100);
+		abbc.addIterative(120);
+	}
+	
+	@Test
+	void addIterativeTest() {
+		setup1();
+		abbc.addIterative(300);
+		assertTrue(abbc.getTop().getAmount() == 300);
+		
+		abbc.addIterative(30);
+		assertTrue(abbc.getTop().getAmount() == 300);
+		assertTrue(abbc.getTop().getLeft().getAmount() == 30);
+		assertTrue(abbc.getTop().getRigth() == null);
+			
+		setup1();
+		abbc.addIterative(10);
+		abbc.addIterative(1250);
+		assertTrue(abbc.getTop().getAmount() == 10);
+		assertTrue(abbc.getTop().getRigth().getAmount() == 1250);
+		assertTrue(abbc.getTop().getLeft() == null);
+	}
+	
+	@Test
+	void addRecursiveTest() {
+		setup1();
+		abbc.addRecursive(300);
+		assertTrue(abbc.getTop().getAmount() == 300);
+		
+		abbc.addRecursive(30);
+		assertTrue(abbc.getTop().getAmount() == 300);
+		assertTrue(abbc.getTop().getLeft().getAmount() == 30);
+		assertTrue(abbc.getTop().getRigth() == null);
+			
+		setup1();
+		abbc.addRecursive(10);
+		abbc.addRecursive(1250);
+		assertTrue(abbc.getTop().getAmount() == 10);
+		assertTrue(abbc.getTop().getRigth().getAmount() == 1250);
+		assertTrue(abbc.getTop().getLeft() == null);
+	}
+	
+	@Test
+	void searchIterativeTest() {
+		setup2();
+		boolean result;
+		
+		result =abbc.searchIterative(20);
+		assertEquals(true, result);
+		
+		result =abbc.searchIterative(40);
+		assertEquals(true, result);
+		
+		result =abbc.searchIterative(60);
+		assertEquals(true, result);
+		
+		result =abbc.searchIterative(80);
+		assertEquals(true, result);
+		
+		result =abbc.searchIterative(100);
+		assertEquals(true, result);
+		
+		result =abbc.searchIterative(10);
+		assertEquals(false, result);
+	}
+	
+	@Test
+	void searchRecursiveTest() {
+		setup2();
+		boolean result;
+		
+		result =abbc.searchRecursive(20);
+		assertEquals(true, result);
+		
+		result =abbc.searchRecursive(40);
+		assertEquals(true, result);
+		
+		result =abbc.searchRecursive(60);
+		assertEquals(true, result);
+		
+		result =abbc.searchRecursive(80);
+		assertEquals(true, result);
+		
+		result =abbc.searchRecursive(100);
+		assertEquals(true, result);
+		
+		result =abbc.searchRecursive(10);
+		assertEquals(false, result);
+	}
+	
+	@Test
+	void deleteIterativeTest() {
+		setup2();
+		boolean result;
+		NodoABB current;
+		
+		assertEquals(20, abbc.getTop().getAmount());
+		result =abbc.deleteIterative(20);
+		assertEquals(true, result);
+		assertEquals(40, abbc.getTop().getAmount());
+		
+		result = abbc.deleteIterative(40);
+		current = abbc.getTop();
+		while(current.getLeft() != null) {
+			current = current.getLeft();
+		}
+		assertEquals(true, result);
+		assertEquals(60, current.getAmount());
+		
+		result = abbc.deleteIterative(80);
+		current = abbc.getTop();
+		while(current.getLeft() != null) {
+			current = current.getLeft();
+		}
+		assertEquals(true, result);
+		assertEquals(60, current.getAmount());
+		
+		result = abbc.deleteIterative(10);
+		assertEquals(false, result);
+	}
+	
+	@Test
+	void deleteRecursiveTest() {
+		setup2();
+		boolean result;
+		NodoABB current;
+		
+		assertEquals(20, abbc.getTop().getAmount());
+		result =abbc.deleteRecursive(20);
+		assertEquals(true, result);
+		assertEquals(40, abbc.getTop().getAmount());
+		
+		result = abbc.deleteRecursive(40);
+		current = abbc.getTop();
+		while(current.getLeft() != null) {
+			current = current.getLeft();
+		}
+		assertEquals(true, result);
+		assertEquals(60, current.getAmount());
+		
+		result = abbc.deleteRecursive(80);
+		current = abbc.getTop();
+		while(current.getLeft() != null) {
+			current = current.getLeft();
+		}
+		assertEquals(true, result);
+		assertEquals(60, current.getAmount());
+		
+		result = abbc.deleteRecursive(10);
+		assertEquals(false, result);
+	}
+}
